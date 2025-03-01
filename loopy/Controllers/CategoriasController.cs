47db81d2a3cdf8b979a9412bdf0d86a3e8b09eb5
Juan Camilo <juancamilomendozavillegas14@gmail.com>
2025-02-28 20:24:38 -0500

    Agregar archivos de proyecto.

---
+++ loopy/Controllers/CategoriasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace loopy.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
